0c594e2f1e428a804cb89277cd3d0308d14c368f
Nagarei <Nagarei@users.noreply.github.com>
2023-11-04 00:11:20 +0900

    初期化

--- webapp/sql/2_NewColumn.sql
+++ webapp/sql/2_NewColumn.sql
@@ -1,2 +1,7 @@
 
 ALTER TABLE `isu_condition` ADD COLUMN `condition_level` VARCHAR(15) NOT NULL DEFAULT "";
+
+
+INSERT INTO `latest_isu_condition` (`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)
+    SELECT * FROM (SELECT `jia_isu_uuid`, MAX(timestamp), `is_sitting`, `condition`, `message` FROM isu_condition GROUP BY jia_isu_uuid)
+	ON DUPLICATE KEY UPDATE timestamp=VALUES(timestamp),is_sitting=VALUES(is_sitting),condition=VALUES(condition),message=VALUES(message)

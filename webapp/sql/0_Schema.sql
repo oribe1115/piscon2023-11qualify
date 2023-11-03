d33615fd5e3c0a79c5f17937153a9cae40e92da6
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-11-03 16:18:19 +0900

    id default 0

--- webapp/sql/0_Schema.sql
+++ webapp/sql/0_Schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE `isu` (
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 CREATE TABLE `isu_condition` (
+  `id` bigint DEFAULT 0,
   `jia_isu_uuid` CHAR(36) NOT NULL,
   `timestamp` DATETIME NOT NULL,
   `is_sitting` TINYINT(1) NOT NULL,

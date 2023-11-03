0d9b0b0954f5b1c69e7f3a019358ee2b25af40c4
Nagarei <Nagarei@users.noreply.github.com>
2023-11-03 11:14:37 +0900

    index

--- webapp/sql/0_Schema.sql
+++ webapp/sql/0_Schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE `isu` (
   `jia_user_id` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-   PRIMARY KEY(`id`)
+   PRIMARY KEY(`id`),
+   INDEX idx_jiaisuuuid_timestamp (`jia_isu_uuid`, `timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 CREATE TABLE `isu_condition` (

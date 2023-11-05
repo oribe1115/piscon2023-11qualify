0e6b0f0b94694339ad554ca6121cdbd097657e38
Nagarei <Nagarei@users.noreply.github.com>
2023-11-05 13:28:37 +0900

    fix init

--- webapp/sql/2_NewColumn.sql
+++ webapp/sql/2_NewColumn.sql
@@ -4,5 +4,5 @@ ALTER TABLE `isu_condition` ADD COLUMN `condition_level` VARCHAR(15) NOT NULL DE
 INSERT INTO `latest_isu_condition` (`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)
 (
     SELECT t.`jia_isu_uuid`, t.`timestamp`, t.`is_sitting`, t.`condition`, t.`message` FROM `isu_condition` AS t
-    JOIN (SELECT `jia_isu_uuid`, MAX(timestamp) FROM isu_condition GROUP BY jia_isu_uuid) AS t2 ON t.`jia_isu_uuid` = t2.`jia_isu_uuid` AND t.`timestamp` = t2.`timestamp`
+    JOIN (SELECT `jia_isu_uuid`, MAX(timestamp) AS `latest_timestamp` FROM isu_condition GROUP BY jia_isu_uuid) AS t2 ON t.`jia_isu_uuid` = t2.`jia_isu_uuid` AND t.`timestamp` = t2.`latest_timestamp`
 );
